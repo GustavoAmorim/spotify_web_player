4c3895b84826eac6d402a20d6544443b526e7e19
Gustavo Amorim <gustavoframorim@hotmail.com>
2022-08-07 13:30:29 -0300

    feat: add lib spotify

--- spotift-web-player/src/app/app.component.ts
+++ spotift-web-player/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import SpotifyWebApi from 'spotify-web-api-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'spotift-web-player';
+
+  ngOnInit(): void {
+
+    const spotifyApi = new SpotifyWebApi();
+  }
 }
+
+
+
